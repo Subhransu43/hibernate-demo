c5cfbc126d73ff3346cbb4e928e556dd89a9df59
Subhransu's PC <Subhransu's PC@LAPTOP-ATOIDJ45>
2021-11-22 22:36:51 +0530

    Adding record to DB

--- Hibernte-demo/src/main/java/com/hibernate/project/App.java
+++ Hibernte-demo/src/main/java/com/hibernate/project/App.java
@@ -1,5 +1,11 @@
 package com.hibernate.project;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.project.entity.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//Create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+        
+        //Create session
+        Session session = factory.getCurrentSession();
+        
+        try {
+        	//Createing student object
+        	Student student = new Student("Subhransu", "Bhandari", "subhransu.bhandari@outlook.com");
+        	System.out.println(student.toString());
+        	
+        	//Start Transction
+        	session.beginTransaction();
+        	
+        	//Save student object
+        	session.save(student);
+        	
+        	//Commit Tracsction
+        	session.getTransaction().commit();
+        	
+        } finally {
+        	factory.close();
+        }
     }
 }
